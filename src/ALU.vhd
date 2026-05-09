ebad5d00618c21b09c49b85b8980435d7f16d63d
marcosagoni21 <225422069+marcosagoni21@users.noreply.github.com>
2026-05-08 23:06:55 -0600

    same cleanup of alu

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -41,36 +41,28 @@ end ALU;
 
 architecture Behavioral of ALU is
     signal w_result : signed(8 downto 0);
-    signal w_op_A : signed(8 downto 0);
-    signal w_op_B : signed(8 downto 0);
+    signal w_sum_unsigned : unsigned(8 downto 0);
 begin
-    w_op_A <= signed(i_A(7) & i_A);
-    w_op_B <= signed(i_B(7) & i_B);
-    process(i_op, w_op_A, w_op_B, i_A, i_B)
+w_result <= signed(i_A(7) & i_A) + signed(i_B(7) & i_B) when i_op = "000" else
+                signed(i_A(7) & i_A) - signed(i_B(7) & i_B) when i_op = "001" else
+                (others => '0');
+w_sum_unsigned <= unsigned('0' & i_A) + unsigned('0' & i_B) when i_op = "000" else
+                      unsigned('0' & i_A) - unsigned('0' & i_B) when i_op = "001" else
+                      (others => '0');
+        process(i_op, i_A, i_B)
     begin
         case i_op is
-            when "000" =>
-            w_result <= w_op_A + w_op_B;
-            when "001" =>
-            w_result <= w_op_A - w_op_B;
-            when "010" =>
-            w_result <= '0' & (signed(i_A) and signed(i_B));
-            when "011" => 
-            w_result <= '0' & (signed(i_A) or signed(i_B));
-            when others =>
-            w_result <= (others => '0');
-           end case;
-           end process;
-           o_result <= std_logic_vector(w_result(7 downto 0));
-           o_flags(2) <= '1' when w_result(7 downto 0) = "00000000" else '0';
-           o_flags(3) <= w_result(7);
-           process(i_op, w_result)
-           begin
-           if i_op = "001" then
-           o_flags(1) <= not w_result(8);
-           else
-           o_flags(1) <= w_result(8);
-           end if;
-           end process;
-           o_flags(0) <= '1' when (i_op = "000" and ((i_A(7) = i_B(7)) and (w_result(7) /=i_A(7) ))) or (i_op = "001" and ((i_A(7) /= i_B(7)) and (w_result(7) /= i_A(7)))) else '0';
+            when "000" => o_result <= std_logic_vector(w_result(7 downto 0));
+            when "001" => o_result <= std_logic_vector(w_result(7 downto 0));
+            when "010" => o_result <= i_A and i_B;
+            when "011" => o_result <= i_A or i_B;
+            when others => o_result <= (others => '0');
+        end case;
+    end process;
+    o_flags(3) <= w_result(7);
+    o_flags(2) <= '1' when w_result(7 downto 0) = "00000000" else '0';
+    o_flags(1) <= w_sum_unsigned(8); 
+    o_flags(0) <= '1' when (i_op = "000" and ((i_A(7) = i_B(7)) and (w_result(7) /= i_A(7)))) or 
+                          (i_op = "001" and ((i_A(7) /= i_B(7)) and (w_result(7) /= i_A(7)))) 
+                  else '0';
 end Behavioral;
