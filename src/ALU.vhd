ec3b1bb7eb4eadcd8a25ef6e9e21b3c3e0174533
marcosagoni21 <225422069+marcosagoni21@users.noreply.github.com>
2026-05-08 23:23:30 -0600

    trying agian

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -30,7 +30,6 @@ use IEEE.NUMERIC_STD.ALL;
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
 entity ALU is
     Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
            i_B : in STD_LOGIC_VECTOR (7 downto 0);
@@ -41,42 +40,53 @@ end ALU;
 
 architecture Behavioral of ALU is
 begin
-process(i_op, i_A, i_B)
-        variable v_result : signed(8 downto 0);
-        variable v_sum_unsigned : unsigned(8 downto 0);
+    process(i_op, i_A, i_B)
+        variable v_result : std_logic_vector(7 downto 0);
+        variable v_temp_math : signed(8 downto 0);
+        variable v_c : std_logic;
+        variable v_v : std_logic;
     begin
+        v_c := '0';
+        v_v := '0';
+
         if i_op = "000" then 
-            v_result := signed(i_A(7) & i_A) + signed(i_B(7) & i_B);
-            v_sum_unsigned := unsigned('0' & i_A) + unsigned('0' & i_B);
-        elsif i_op = "001" then 
-            v_result := signed(i_A(7) & i_A) - signed(i_B(7) & i_B);
-            if unsigned(i_A) >= unsigned(i_B) then
-                v_sum_unsigned := "100000000";
-            else
-                v_sum_unsigned := "000000000"; 
+            v_temp_math := signed(i_A(7) & i_A) + signed(i_B(7) & i_B);
+            v_result := std_logic_vector(v_temp_math(7 downto 0));
+            v_c := std_logic(unsigned('0' & i_A) + unsigned('0' & i_B))(8);
+            if (i_A(7) = i_B(7)) and (v_result(7) /= i_A(7)) then
+                v_v := '1';
             end if;
+        elsif i_op = "001" then 
+            v_temp_math := signed(i_A(7) & i_A) - signed(i_B(7) & i_B);
+            v_result := std_logic_vector(v_temp_math(7 downto 0));
+            if unsigned(i_A) >= unsigned(i_B) then
+                v_c := '1';
+            else
+                v_c := '0';
+            end if;
+            if (i_A(7) /= i_B(7)) and (v_result(7) /= i_A(7)) then
+                v_v := '1';
+            end if;
+        elsif i_op = "010" then
+            v_result := i_A and i_B;
+        elsif i_op = "011" then
+            v_result := i_A or i_B;
         else
             v_result := (others => '0');
-            v_sum_unsigned := (others => '0');
         end if;
-        case i_op is
-            when "000" | "001" => o_result <= std_logic_vector(v_result(7 downto 0));
-            when "010"         => o_result <= i_A and i_B;
-            when "011"         => o_result <= i_A or i_B;
-            when others        => o_result <= (others => '0');
-        end case;
-        o_flags(3) <= v_result(7); 
-        if v_result(7 downto 0) = "00000000" then 
+
+        o_result <= v_result;
+
+        o_flags(3) <= v_result(7);
+        
+        if v_result = "00000000" then
             o_flags(2) <= '1';
         else
             o_flags(2) <= '0';
         end if;
-        o_flags(1) <= v_sum_unsigned(8); 
-        if (i_op = "000" and ((i_A(7) = i_B(7)) and (v_result(7) /= i_A(7)))) or 
-           (i_op = "001" and ((i_A(7) /= i_B(7)) and (v_result(7) /= i_A(7)))) then
-            o_flags(0) <= '1'; 
-        else
-            o_flags(0) <= '0';
-        end if;
+
+        o_flags(1) <= v_c;
+        o_flags(0) <= v_v;
+
     end process;
 end Behavioral;
